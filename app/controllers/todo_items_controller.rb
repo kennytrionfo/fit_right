0c7b828c9cfdb1da89210dc0e5d28f90622bddb7
kennytrionfo <kentrionfo@gmail.com>
2015-02-05 20:00:39 -0700

    more step by step

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,2 +1,9 @@
 class TodoItemsController < ApplicationController
+
+	def create
+		@todo_list = TodoList.find(params[:todo_list_id])
+		@todo_item = @todo_list.todo_items.create(params[:todo_item].permit(:content))
+	end
+
+
 end
