4563abb850dde5f9e215b7cc081600d62796cc89
kennytrionfo <kentrionfo@gmail.com>
2015-02-13 08:00:37 -0700

    tryign to fix show page error

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,16 +1,16 @@
 class TodoItemsController < ApplicationController
 #This was the create method before putting in before_action and private methods in below. 
-	def create
-		@todo_list = TodoList.find(params[:todo_list_id])
-		@todo_item = @todo_list.todo_items.create(params[:todo_item].permit(:content))
-	end
+	# def create
+	# 	@todo_list = TodoList.find(params[:todo_list_id])
+	# 	@todo_item = @todo_list.todo_items.create(params[:todo_item].permit(:content))
+	# end
 
 #After updating the create method with before_action and private methods. 
 #————————Kenny Trionfo Feb 11,2015————
 	before_action :set_todo_list
 
 	def create
-		@todo_item = @todo_list.todo_item.create(todo_item_params)
+		@todo_item = @todo_list.todo_items.create(todo_item_params)
 		redirect_to @todo_list 
 	end
 
